38c35697f4a796a8ef8b845c60b96f5adcb95bab
anthony decostanzo <anthony@ascent.ai>
2018-04-16 16:09:29 +0900

    Minor cleaning up of main.

--- tutorials/01-basics/feedforward_neural_network/main.py
+++ tutorials/01-basics/feedforward_neural_network/main.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 from torch.autograd import Variable
+from torch.utils.data import DataLoader
 
 
 # Hyper Parameters 
@@ -21,21 +22,31 @@ train_dataset = dsets.MNIST(root='./data',
 
 test_dataset = dsets.MNIST(root='./data', 
                            train=False, 
-                           transform=transforms.ToTensor())
+                           transform=transforms.ToTensor(),
+                           download=True)
 
 # Data Loader (Input Pipeline)
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
-                                           batch_size=batch_size, 
+# train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+#                                            batch_size=batch_size,
+#                                            shuffle=True)
+#
+# test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+#                                           batch_size=batch_size,
+#                                           shuffle=False)
+train_loader = DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
                                            shuffle=True)
 
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
-                                          batch_size=batch_size, 
+test_loader  = DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
                                           shuffle=False)
+
+
 
 # Neural Network Model (1 hidden layer)
 class Net(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
-        super(Net, self).__init__()
+        super(Net, self).__init__()  #python 2 syntax requires 2 arguments for super, in python 3, no arguments are required
         self.fc1 = nn.Linear(input_size, hidden_size) 
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(hidden_size, num_classes)  
@@ -80,7 +91,7 @@ for images, labels in test_loader:
     _, predicted = torch.max(outputs.data, 1)
     total += labels.size(0)
     correct += (predicted == labels).sum()
-    print("total: ", total, "correct: ", correct)
+    print("Test set, correct/total:", correct/total)
 
 print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
 
